84077fa13a9bd8d15b1f6236e79e587f2f1fecfb
hamir01 <hamirc93@gmail.com>
2021-10-14 11:46:13 -0500

    maquetacion

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
-function cambiarClase(){
-    let siteNav = document.getElementById('site-nav');
-        siteNav.classList.toggle('site-nav-open');
-    let menuOpen = document.getElementById('menu-toggle');
-        menuOpen.classList.toggle('menu-open');    
-        
+function mostrarMenu (){
+    var navMostrar = document.getElemenById("menu-principal");
+    navMostrar.claaList.toggle("mostrarNav");
+
 }
