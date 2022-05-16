b530e29e4cbf0651d0c36db6212210dfb62aa890
hamir_ortiz <hamircQqQc93@gmail.com>
2022-05-15 22:07:38 -0500

    agregando netflix

---
+++ netflix/js/cartelera.js
@@ -0,0 +1,50 @@
+function volver(){
+    document.getElementById("imgprincipal").src= "img/netflix.jpg";
+}
+
+function intercambiarbehold(){
+    document.getElementById("imgprincipal").src= "img/behild1.jpg";
+    
+}
+function intercambiarcertain(){
+    document.getElementById("imgprincipal").src= "img/Certain1.jpg";
+}
+function intercambiargreen(){
+    document.getElementById("imgprincipal").src= "img/green-room1.jpg";
+}
+function intercambiarlove(){
+    document.getElementById("imgprincipal").src= "img/love1.jpg";
+}
+function intercambiarmoonlight(){
+    document.getElementById("imgprincipal").src= "img/moonlight1.jpg";
+}
+function intercambiarnatalie(){
+    document.getElementById("imgprincipal").src= "img/natalie1.jpg";
+}
+function intercambiarshine(){
+    document.getElementById("imgprincipal").src= "img/shine1.jpg";
+}
+function intercambiarthe(){
+    document.getElementById("imgprincipal").src= "img/lo1.jpeg";
+}
+function intercambiartic(){
+    document.getElementById("imgprincipal").src= "img/tickl1.png";
+}
+function intercambiarextra(){
+    document.getElementById("imgprincipal").src= "img/extra1.jpg";
+}
+function intercambiarfree(){
+    document.getElementById("imgprincipal").src= "img/free1.jpg";
+}
+function intercambiargoat(){
+    document.getElementById("imgprincipal").src= "img/goat1.jpg";
+}
+function intercambiaroperation(){
+    document.getElementById("imgprincipal").src= "img/operation1.jpg";
+}
+function intercambiarnation(){
+    document.getElementById("imgprincipal").src= "img/nation1.jpg";
+}
+function intercambiarrise(){
+    document.getElementById("imgprincipal").src= "img/rise1.jpg";
+}
